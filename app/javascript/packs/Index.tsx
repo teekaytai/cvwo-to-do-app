066b2403f157612c03ca62aee5a3e1a8add6c1f9
teekaytai <taitzekin@gmail.com>
2022-01-17 18:57:35 +0800

    Added support for Internet Explorer

--- app/javascript/packs/Index.tsx
+++ app/javascript/packs/Index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+import * as React from "react";
 import { render } from "react-dom";
 import App from "../components/App";
 
